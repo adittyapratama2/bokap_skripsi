9cf8507f8a112d170fe23f9ea257feac8c21c9a7
adittya0015 <adittyapratama2@gmail.com>
2020-02-29 07:24:00 +0700

    Commit 2

---
+++ resources/js/components/Customer.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Data Pelanggan</h3>
+
+                    <div class="card-tools">
+                        <button class="btn btn-success" data-toggle="modal"
+                        data-target="#addNew">Tambah Baru</button>
+                    </div>
+
+                </div>
+
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-hover">
+                        <tbody>
+                            <tr>
+                                <th>Kode</th>
+                                <th>Wilayah</th>
+                                <th>Nama Toko</th>
+                                <th>Alamat</th>
+                                <th>Aksi</th>
+                            </tr>
+                            <tr>
+                                <td>XXX</td>
+                                <td>XXX</td>
+                                <td>XXX</td>
+                                <td>XXX</td>
+                                <td>
+                                    <a href="#">
+                                        <i class="fa fa-edit"></i>
+                                    </a>
+
+                                    <a href="#">
+                                        <i class="fa fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        </div>
+
+        <!-- Modal -->
+<div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addNewLabel">Pelanggan Baru</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Tutup</button>
+        <button type="button" class="btn btn-success">Buat Baru</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        loadCustomers(){
+            axios
+        }
+    },
+    created() {
+        this.loadCustomers();
+    }
+}
+</script>
